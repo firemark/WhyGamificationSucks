41659c6d25d477b507087a44f2b594acd80d96a8
Firemark <marpiechula@gmail.com>
2014-07-22 22:46:24 +0200

    Shuffle commits

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ while true; do
 	for i in {0..2}; do
 		echo $(date) >> firemark
 		git add firemark
-		git commit -am 'gamification sucks'
+		git commit -am "$(shuf -n 1 commits)"
 		sleep 60
 	done
 	git push origin master
